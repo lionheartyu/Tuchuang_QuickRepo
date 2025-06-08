c25979722b55e856c6b6b0c9cdb8dcc01ccae754
KK_lion <545258830@qq.com>
2025-06-08 14:53:21 +0800

    Delete tc-src directory

--- tc-src/api/ApiSharefiles.h
+++
@@ -1,8 +0,0 @@
-#ifndef _API_SHAREFILES_H_
-#define _API_SHAREFILES_H_
-#include <string>
-using std::string;
-;
-int ApiSharefiles(string &url, string &post_data, string &str_json);
-
-#endif
--- tc-src/api/ApiSharepicture.h
+++
@@ -1,8 +0,0 @@
-#ifndef _API_SHAREPICTURE_H_
-#define _API_SHAREPICTURE_H_
-#include <string>
-using std::string;
-;
-int ApiSharepicture(string &url, string &post_data, string &str_json);
-
-#endif
